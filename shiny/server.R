580096d17e9a6af834d98614f617adb2a837218d
Anne Driscoll <amd112@duke.edu>
2018-04-11 17:14:49 -0400

    fixed a lot of bugs, added data checks

--- shiny/server.R
+++ shiny/server.R
@@ -5,6 +5,8 @@ library(curl)
 library(ggmap)  
 library(rgdal)
 library(readr)
+library(data.table)
+library(stringr)
 
 #TODO: profiling shiny apps
 #TODO: pass tract data when zoom is > 11
@@ -12,22 +14,25 @@ library(readr)
 #TODO: create a warning thing for when geolocation fails.
 #TODO: figure out how to change latlon as the map is shifted
 
-data_county = read.csv("../index/data/toxic/toxic_2000_2010_county.csv")
-data_tract = read.csv("../index/data/toxic/tract/toxic_2000_2010_tract.csv")
-race_tract = read.csv("../index/data/census/race/race_tract_2000.csv")
+data_county = fread("../index/data/toxic/toxic_2000_2010_county.csv")
+data_tract = fread("../index/data/toxic/tract/toxic_2000_2010_tract.csv")
+race_tract = fread("../index/data/census/race/race_tract_2000.csv")
 #1000 on readin
 names(data_county) = c("county", "tox")
 names(data_tract) = c("tract", "tox", "area")
+race_tract$id = str_pad(race_tract$id, 11, "left", pad = "0")
 
-data_tract = merge(data_tract, race_tract, by.x = "tract", by.y = "id") #300 ms
+data_tract = merge(data_tract, race_tract, by.x = "tract", by.y = "id", all = TRUE) #300 ms
+data_tract[is.na(data_tract$tox), "tox"] = 1*10^-6
 
 counties = readOGR("cb_2016_us_county_20m.shp", layer = "cb_2016_us_county_20m", GDAL1_integer64_policy = TRUE) #3000ms
+counties = subset(counties, !(counties$STATEFP %in% c("15", "02", "72")))
 counties@data = counties@data[, c(1, 5, 6, 8)]
-counties@data$GEOID = as.numeric(as.character(counties@data$GEOID))
-counties@data$STATEFP = as.numeric(as.character(counties@data$STATEFP))
-counties@data =data.frame(counties@data, data_county[match(counties@data[,"GEOID"], data_county[,"county"]),])
+counties@data$GEOID = as.character(counties@data$GEOID)
+counties@data$STATEFP = as.character(counties@data$STATEFP)
+counties@data =data.frame(counties@data, data_county[match(counties@data$GEOID, data_county$county), ])
+counties@data$tox[is.na(counties@data$tox)] = 1*(10^-6)
 
-#lower48 = subset(counties, !(counties$STATEFP %in% c("15", "02", "72")))
 matrix = read.csv("state_adjacent.csv", header = FALSE)
 nc = subset(counties, counties$STATEFP %in% c("37", "51", "47", "13", "45"))
 p = "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
@@ -37,32 +42,42 @@ pal = colorNumeric("magma", log(counties@data$tox) * abs(log(counties@data$tox))
 
 function(input, output, session) {
   
-  #get the lat/long reactively upon the user searching
-  latlon = eventReactive(input$search, {geocode(input$addressInput, output = "latlon", source = "dsk")})
+  values = reactiveValues(
+    latlon = c(-78.94001, 36.00153), 
+    current_c = SpatialPoints(matrix(c(-78.94001, 36.00153), nrow = 1), proj4string = CRS(p)) %over% counties 
+  )
+  data = reactiveValues(
+    tract = data_tract[startsWith(as.character(data_tract$tract), as.character(isolate(values$current_c$GEOID))), ]
+  )
+  print(isolate(values$latlon))
+  print(isolate(values$ll))
+  print(isolate(values$current_c))
+  print(isolate(values$current_c$GEOID))
+  print(isolate(data$tract[1:5, ]))
+
+  observeEvent(input$search, {
+    print(data$tract[1, ])
+    values$latlon = geocode(input$addressInput, output = "latlon", source = "dsk")
+    print(values$latlon)
+    values$ll = SpatialPoints(matrix(as.numeric(values$latlon), nrow = 1), proj4string = CRS(p))
+    print(values$ll)
+    values$current_c = values$ll %over% counties
+    print(values$current_c$GEOID)
+    data$tract = data_tract[startsWith(as.character(data_tract$tract), as.character(values$current_c$GEOID)), ]
+    print(data$tract[1, ])
+  })
   
-  #create the SpatialPoint based on lat/long
-  ll = reactive({SpatialPoints(latlon(), proj4string = CRS(p))})
-  
-  #get the Spatial Polygon that coresponds to the correct location
-  current_c = reactive({ll() %over% counties})
-  
-  tract = reactive({data_tract[startsWith(as.character(data_tract$tract), as.character(current_c()$GEOID)), ]})
- 
-  adjacent_list = reactive({ 
-    unlist(matrix[matrix$V1 == current_c()$STATEFP, ], use.names = FALSE)[!is.na(unlist(matrix[matrix$V1 == current_c()$STATEFP, ], use.names = FALSE))]
-  }) #weirdly slow 500ms
-   
   map_data = reactive({ 
     #print(adjacent_list())
     subset(counties, counties@data$STATEFP %in% adjacent_list())
   })
   
   observeEvent(input$search, {
-    leafletProxy("map", session, data = map_data()) %>%
+    leafletProxy("map", session) %>%
       #addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5, opacity = 0.5, 
       #            fillOpacity = 0.8, fillColor = ~pal(log(tox) * abs(log(tox)))
       #) %>%
-      setView(lng = latlon()[1], lat = latlon()[2], zoom = 9) 
+      setView(lng = values$latlon[1], lat = values$latlon[2], zoom = 9)
   })
   
   output$map = renderLeaflet({
@@ -78,12 +93,19 @@ function(input, output, session) {
   })
   
   output$national = renderPlot({
-    state = as.numeric(substr(current_c()$GEOID, 1, nchar(current_c()$GEOID) - 3))
+    validate(
+      need(nrow(data$tract)>0, "No data for this area."),
+      need(!anyNA(data$tract$concentration), "No data for this area.")
+    )
+    comp = complete.cases(data$tract[, c(1:2, 4:11)])
+    temp = data$tract[comp, ]
+    
+    state = substr(values$current_c$GEOID, 1, nchar(values$current_c$GEOID) - 3)
     state_tox = subset(counties, counties@data$STATEFP == state)@data$tox
-    ggplot() + geom_vline(xintercept = current_c()$tox, linetype = "longdash", alpha = 0.5) +
-      geom_line(aes(counties$tox, color = "Nation"), stat = "density") + 
+    ggplot() + geom_vline(xintercept = values$current_c$tox, linetype = "longdash", alpha = 0.5) +
+      geom_line(aes(counties@data$tox, color = "Nation"), stat = "density") + 
       geom_line(aes(state_tox, color = "State"), stat = "density") +
-      geom_line(aes(tract()$tox, color = "Within County"), stat = "density") +
+      geom_line(aes(temp$tox, color = "Within County"), stat = "density") +
       theme_classic() +
       theme(legend.position = "bottom", axis.line.y = element_line(color = "grey"), axis.line.x = element_line(color = "grey")) + 
       xlab("Log Toxicity") +
@@ -99,8 +121,14 @@ function(input, output, session) {
   }) 
   
   output$race = renderPlot({
-    ggplot() + geom_density(aes(tract()$tox, weight = tract()$black/sum(tract()$black), fill = "black"), alpha = 0.4 , color = NA) + xlab("Log Toxicity") +
-      geom_density(aes(tract()$tox, weight = tract()$white/sum(tract()$white), fill = "white"), alpha = 0.4 , color = NA) +
+    validate(
+      need(nrow(data$tract)>0, "No data for this area."),
+      need(!anyNA(data$tract$concentration), "No data for this area.")
+    )
+    comp = complete.cases(data$tract[, c(1:2, 4:11)])
+    temp = data$tract[comp, ]
+    ggplot() + geom_density(aes(temp$tox, weight = temp$black/sum(temp$black), fill = "black"), alpha = 0.4 , color = NA) + xlab("Log Toxicity") +
+      geom_density(aes(temp$tox, weight = temp$white/sum(temp$white), fill = "white"), alpha = 0.4 , color = NA) +
       theme_classic() +
       theme(legend.position = "bottom", axis.line.y = element_line(color = "grey"), axis.line.x = element_line(color = "grey")) +
       scale_x_log10(breaks = scales::trans_breaks("log10", function(x) 10^x),
